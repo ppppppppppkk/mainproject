5e9364e12d0a34a77cd3e7bf12f63f1f13db22b9
[lococo0425] <[lococo0425@naver.com]>
2023-12-27 16:15:42 +0900

    12.27. v2

--- main/js/김동훈js/payment.js
+++ main/js/김동훈js/payment.js
@@ -92,3 +92,20 @@ function successPayment(){
     }
 
 }
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const select = document.querySelector('select')
+    const p = document.querySelector('p')
+
+    select.addEventListener('change', function(event){
+        const options = event.currentTarget.options
+        const list = []
+        for (const option of options) {
+            if(option.selected){
+                list.push(option.textContent)
+            }
+        }
+        p.textContent = `가게 요청사항: ${list}`
+    })
+})
